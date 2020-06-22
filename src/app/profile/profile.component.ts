31bc45e5d8de0c8500f1ec7f7017ab967a5fc410
Gr3atJes <mathiujes@gmail.com>
2020-06-22 05:09:25 +0300

    Changing edit of form group and import of component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,33 +1,38 @@
+import { ProfileService } from './../profile.service';
 import { Component, OnInit } from '@angular/core';
-import { ProfileService } from '../../services/profile.service';
+
+
 @Component({
-  selector: 'gg-profile',
+  selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  profile:any[];
-  repos:any[];
-  username:string;
+   profile: any;
+   repos: any;
+   username: any = 'Eugeneiregi';
 
-  
+
   constructor(private profileService: ProfileService) {
+
+  }
+
+
+  findProfile() {
+    this.profileService.updateProfile(this.username);
     this.profileService.getProfileInfo().subscribe(profile => {
       console.log(profile);
       this.profile = profile;
-    });
-
+});
     this.profileService.getProfileRepos().subscribe(repos => {
-      console.log(repos);
-      this.repos = repos;
-    })
-   }
+  console.log();
+  this.repos = repos;
+});
 
-   findProfile(){
-     this.profileService.updateProfile(this.username)
-   }
+  }
 
   ngOnInit(): void {
+    this.findProfile();
   }
 
 }
